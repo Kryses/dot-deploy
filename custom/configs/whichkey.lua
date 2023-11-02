92a964a843e230d22423190d954db987e796fd74
Chad Provencher <thechad802@gmail.com>
2023-11-02 15:19:16 -0400

    More neorg configuration

--- custom/configs/whichkey.lua
+++ custom/configs/whichkey.lua
@@ -8,18 +8,27 @@ wk.register {
     },
     s = { name = "settings..."},
     r = { name = "refactor..."},
-    f = { name = "find..."},
+    f = { name = "find...",
+    },
     p = { name = "project..."},
     n = { name = 'neorg...',
       {
+        w = {'<cmd>Telescope neorg switch_workspace<cr>', 'workspace'},
         i = {"<cmd>Neorg index<cr>", 'index'},
         t = {name = 'template...',
           {
             a = {':Neorg templates add ', 'add'},
             i = {':Neorg templates fload ', 'insert'},
-          }
+          },
+        },
+        l = {name='link..',
+          f = {'<cmd>Telescope neorg insert_file_link<cr>', 'file_link'},
+          l = {'<cmd>Telescope neorg insert_link<cr>', 'link'}
+        },
+        j = {
+          name = 'journal...',
+          t = {'<cmd>Neorg journal today<cr>', 'today'}
         }
-
       }
     }
   },
--- custom/plugins.lua
+++ custom/plugins.lua
@@ -135,29 +135,48 @@ local plugins = {
         "pysan3/neorg-templates",
         dependencies = {
           "L3MON4D3/LuaSnip"
-        }
-      }
+        },
+      },
+      { "nvim-neorg/neorg-telescope"},
+      { "pritchett/neorg-capture"}
     },
     config = function()
       require("neorg").setup ({
         load = {
-          ["core.defaults"] = {
-
-
-          }, -- Loads default behaviour
+          ["core.defaults"] = {}, -- Loads default behaviour
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.journal"] = {
+            config = {
+              strategy = "flat"
+            }
+          },
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
-              default_workspace = 'refile',
+              default_workspace = 'kryses',
               use_popup = true,
               workspaces = {
-                work = "~/notes/work",
-                home = "~/notes/home",
-                refile = "~/notes/refile"
+                kryses = "~/notes/kryses",
               },
             },
           },
-          ["external.templates"] = { '~/.config/nvim/lua/custom/templates/norg'}
+          ["external.templates"] = {},
+          ["external.capture"] = {
+            config = {
+              templates = {
+                description = "Default Capture",
+                name = 'default',
+                file = 'refile.norg',
+                enabled = function()
+                  return true
+                end,
+                datetree = true,
+                headline = "Capture",
+                path = {"Save", "refile"},
+                query = "(headline1) @neorg-capture-target"
+              }
+            }
+          },
+          ["core.integrations.telescope"] = {},
         },
       })
     end,
